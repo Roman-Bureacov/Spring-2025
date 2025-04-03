ec3177e912f50b56ecd38bfd6f18e50d08a18fe0
RomaBureacov <RomanBureacov@gmail.com>
2025-04-03 00:59:25 -0700

    javadoc

--- tests/Week_01/MySetTest.java
+++ tests/Week_01/MySetTest.java
@@ -17,154 +17,186 @@ import uw.tcss.TCSS_342.Week_01.src.Set;
  */
 public class MySetTest {
 
-    private static Set fSet;
+    private static Set sfSet;
 
+    /**
+     * Creates a new set for each test.
+     */
     @BeforeEach
     public void setup() {
-        fSet = new MySet(0);
+        sfSet = new MySet(0);
     }
 
+    /**
+     * Tests on isEmpty returning the right boolean for no elements and
+     * with an element.
+     */
     @Test
     public void testIsEmpty() {
         assertTrue(
-                fSet.isEmpty(),
+                sfSet.isEmpty(),
                 "set did not return empty when first instantiated"
         );
-        fSet.insert(new Object());
+        sfSet.insert(new Object());
         assertFalse(
-                fSet.isEmpty(),
+                sfSet.isEmpty(),
                 "set did not return is not empty when inserting element"
         );
     }
 
+    /**
+     * test on makeEmpty. Tests if a set will consider itself empty propery after
+     * making empty and if it will throw exceptions on successive calls.
+     */
     @Test
     public void testMakeEmpty() {
         assertTrue(
-                fSet.isEmpty(),
+                (new MySet(100)).isEmpty(),
+                "constructed set with 100 spaces erroneously considered "
+                        + "non-emtpy"
+        );
+        assertTrue(
+                sfSet.isEmpty(),
                 "Set was not empty on instantiation"
         );
-        fSet.insert(new Object());
+        sfSet.insert(new Object());
         assertFalse(
-                fSet.isEmpty(),
+                sfSet.isEmpty(),
                 "set was considered empty after adding element"
         );
         assertDoesNotThrow(
-                () -> fSet.makeEmpty(),
+                () -> sfSet.makeEmpty(),
                 "set threw exception on make empty"
         );
         assertTrue(
-                fSet.isEmpty(),
+                sfSet.isEmpty(),
                 "set is not considered empty after making empty"
         );
         assertDoesNotThrow(
-                () -> fSet.makeEmpty(),
+                () -> sfSet.makeEmpty(),
                 "set threw exception on making empty after making empty"
         );
     }
 
+    /**
+     * Test for feeding null into insert and remove
+     */
     @Test
     public void testNull() {
         assertThrows(
                 NullPointerException.class,
-                () -> fSet.insert(null),
+                () -> sfSet.insert(null),
                 "set did not throw correct exception on insertion of null"
         );
         assertThrows(
                 NullPointerException.class,
-                () -> fSet.remove(null),
+                () -> sfSet.remove(null),
                 "set did not throw correct exception of remove null"
         );
     }
 
+    /**
+     * Test for inserting various objects and removing throwing exceptions.
+     */
     @Test
     public void testInsertion() {
         final Object lObj = new Object();
         assertDoesNotThrow(
-                () -> fSet.insert(lObj),
+                () -> sfSet.insert(lObj),
                 "set threw an exception upon insertion of an object"
         );
         assertDoesNotThrow(
-                () -> fSet.remove(lObj),
+                () -> sfSet.remove(lObj),
                 "set threw an exception upon attempting to remove an existing object"
         );
         assertDoesNotThrow(
-                () -> fSet.remove(new Object()),
+                () -> sfSet.remove(new Object()),
                 "set threw an exception when attempting to remove an object not in set"
         );
         assertThrows(
                 NullPointerException.class,
-                () -> fSet.remove(null),
+                () -> sfSet.remove(null),
                 "set did not throw correct exception for remove null"
         );
     }
 
+    /**
+     * Test if the set will properly resize when adding enough elements.
+     */
     @Test
     public void testResize() {
         assertEquals(
                 0,
-                fSet.size(),
+                sfSet.size(),
                 "set did not construct with size 0"
         );
 
         final int lNumberOfInsertions = 11;
         for (int i = 0; i < lNumberOfInsertions; i++) {
-            fSet.insert(new Object());
+            sfSet.insert(new Object());
             assertEquals(
                     i + 1,
-                    fSet.size(),
+                    sfSet.size(),
                     "set did not increment when adding an item"
             );
         }
     }
 
+    /**
+     * tests on the size method, if the size method returns the right amount
+     * and if duplicates change the size at all.
+     */
     @Test
     public void testSize() {
         assertEquals(
                 0,
-                fSet.size(),
+                sfSet.size(),
                 "set did not return 0 for a new set"
         );
-        fSet.insert(new Object());
+        sfSet.insert(new Object());
         assertEquals(
                 1,
-                fSet.size(),
+                sfSet.size(),
                 "set did not register an increase in size"
         );
         final Object lOther = new Object();
-        fSet.insert(lOther);
+        sfSet.insert(lOther);
         assertEquals(
                 2,
-                fSet.size(),
+                sfSet.size(),
                 "set did not register another increase in size"
         );
-        fSet.insert(lOther);
+        sfSet.insert(lOther);
         assertEquals(
                 2,
-                fSet.size(),
+                sfSet.size(),
                 "set incorrectly changes size when attempting to insert a duplicate"
         );
-        fSet.remove(lOther);
+        sfSet.remove(lOther);
         assertEquals(
                 1,
-                fSet.size(),
+                sfSet.size(),
                 "set did not register removing an object in size"
         );
     }
 
+    /**
+     * tests on isPresent.
+     */
     @Test
     public void testPresent() {
         assertFalse(
-                fSet.isPresent(null),
+                sfSet.isPresent(null),
                 "set did not register that null is not present"
         );
         final Object lObj = new Object();
-        fSet.insert(lObj);
+        sfSet.insert(lObj);
         assertTrue(
-                fSet.isPresent(lObj),
+                sfSet.isPresent(lObj),
                 "set did not recognize a present object"
         );
         assertFalse(
-                fSet.isPresent(new Object()),
+                sfSet.isPresent(new Object()),
                 "set failed to not recognize an object not in the set"
         );
     }
